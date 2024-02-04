6109e50efdc63bb4cb1c0eaaa626496e34a70bb3
Aurélien Leleu <33595582+AurelienLELEU@users.noreply.github.com>
2024-02-04 17:12:02 +0100

    Update Navbar.js

--- React/src/components/Navbar.js
+++ React/src/components/Navbar.js
@@ -4,18 +4,21 @@ import './App.css';
 import ItemList from './Items';
 
 function Navbar() {
-    const [searchValue, setSearchValue] = useState('');
-    const [currencyValue, setCurrencyValue] = useState("EUR");
+    const [searchValue, setSearchValue] = useState(''); //useState pour gérer la valeur de recherche
+    const [currencyValue, setCurrencyValue] = useState("EUR"); //useState pour gérer la devise sélectionnée
     let currencies = ["EUR", "USD", "JPY", "GBP", "AUD"];
-
+    
+    // Gestionnaire d'événement pour mettre à jour la valeur de recherche
     const handleInputChange = (event) => {
         setSearchValue(event.target.value);
     };
-
+    
+    // Gestionnaire d'événement pour mettre à jour la devise sélectionnée
     const handleInputCurrency = (event, selectedCurrency) => {
         setCurrencyValue(selectedCurrency);
     };
-
+    
+    // Rendu du composant Navbar
     return (
         <div className="ew">
             <div className="Navbar">
